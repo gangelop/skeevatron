9feb5f6e8209a894fc5a54f94aa6aba9e71939a3
George Angelopoulos <george@usermod.net>
2021-11-22 15:41:01 +0200

    be less nice

--- skeevatron.sh
+++ skeevatron.sh
@@ -15,5 +15,5 @@ function dlandunzip() {
 while IFS= read -r id; do
     finalurl="$downloadpage""$id"/
     dlandunzip "$finalurl"
-    sleep 0.5 # be nice
+    sleep 0.2 # be nice
 done < <(cut -f1 -d' ' "$addonslist")
