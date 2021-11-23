ed64c4e348f9a07fe6e71947b3415a283ec6ba57
George Angelopoulos <george@usermod.net>
2021-11-23 08:58:03 +0200

    simplify

--- skeevatron.sh
+++ skeevatron.sh
@@ -4,7 +4,6 @@ addondir="$HOME/documents/Elder Scrolls Online/live/AddOns/"
 addonslist="$addondir/addons"
 downloadpage="https://cdn.esoui.com/downloads/file"
 
-# expects $1 == finalurl
 function dlandunzip() {
     url="$1"
     f=$(mktemp /tmp/skeevatron.XXXXXX)
@@ -13,7 +12,6 @@ function dlandunzip() {
 }
 
 while IFS= read -r id; do
-    finalurl="$downloadpage""$id"/
-    dlandunzip "$finalurl"
+    dlandunzip "${downloadpage}${id}/"
     sleep 0.2 # be nice
 done < <(cut -f1 -d' ' "$addonslist")
